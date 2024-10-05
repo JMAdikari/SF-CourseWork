a62a16b0a67d74585f5989dbd3bd9ab1b7e2a997
JMAdikari <jayaniadikari19@gmail.com>
2024-10-05 21:33:03 +0530

    DB update

--- web/DB/quickplate.sql
+++ web/DB/quickplate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 14, 2024 at 11:29 AM
+-- Generation Time: Oct 05, 2024 at 06:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -45,15 +45,6 @@ CREATE TABLE `cart` (
   `image` varchar(255) NOT NULL,
   `quantity` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `cart`
---
-
-INSERT INTO `cart` (`id`, `name`, `price`, `image`, `quantity`) VALUES
-(14, 'asdfgh', '67', 'pexels-valeriya-1639557.jpg', 1),
-(15, 'asdfghtgg', '46', 'pexels-foodie-factor-162291-551991.jpg', 1),
-(16, 'asdfghfgh', '432', 'pexels-foodie-factor-162291-551991.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -108,7 +99,7 @@ CREATE TABLE `order` (
 --
 
 INSERT INTO `order` (`id`, `name`, `number`, `email`, `method`, `flat`, `street`, `city`, `state`, `country`, `pin_code`, `total_products`, `total_price`) VALUES
-(1, 'jayani adikari', '0741158604', 'jayaniadikari19@gmail.com', 'cash on delivery', '385-A/4, new road', 'dtjcykjhlk', 'wennappuwa', 'srilanka', 'giukjghi', 61170, 'asdfghfgh (1) , asdfgh (1) , asdfghtgg (1) ', '545');
+(35, 'jayani adikari', '0741158604', 'jayaniadikari19@gmail.com', 'paypal', '385-A/4, new road', 'dtjcykjhlk', 'wennappuwa', 'srilanka', 'grzbd', 61170, 'Seafood Pizza (1) , Tandoori Chicken Pizza (1) , BBQ Pizza (1) , Devilled Chicken Pizza (1) , Club Sandwich (1) , Chicken Pasta (1) , Cappuccino (1) , Sparkling Mint Limeade (1) , Fish Tacos (1) , Shrimp Scampi (1) ', '111');
 
 -- --------------------------------------------------------
 
@@ -128,10 +119,15 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `image`) VALUES
-(2, 'asdfgh', '67', 'pexels-valeriya-1639557.jpg'),
-(3, 'asdfghtgg', '46', 'pexels-foodie-factor-162291-551991.jpg'),
-(4, 'asdfghfgh', '432', 'pexels-foodie-factor-162291-551991.jpg'),
-(5, 'asdfghtgg', '4555', '_7fdf5a5a-f583-4830-b35c-1436bfe352c8.jpeg');
+(27, 'Pizza', '45', 'https://cdn.tasteatlas.com/images/dishes/b05a0af72ad845f3a6abe16143d7853a.jpg?mw=2000'),
+(28, 'Japanese Noodles', '36', 'https://www.healthygffamily.com/wp-content/uploads/2019/12/A516112C-3492-4BB4-852C-D51B22C49232-1536x1536.jpg'),
+(29, 'Crispy Burger', '39', 'https://flaevor.com/wp-content/uploads/2020/09/BaconEggCheeseBurger-1.jpg'),
+(30, 'French Fries', '20', 'https://goldenfingers.us/wp-content/uploads/2020/03/french_fry.jpg'),
+(31, 'Chicken Nuggets', '39', 'https://thetoastedpinenut.com/wp-content/uploads/2017/08/IMG_5288.jpg'),
+(32, 'BBQ', '45', 'https://feastio.com/wp-content/uploads/2023/01/Goldees-BBQ-tray.jpg'),
+(33, 'Tandoori Chicken and Naan', '40', 'https://images.halaal.recipes/06-02-18/2018-02-06-10-54-10-86JQl.jpg'),
+(34, 'Chicken Pasta with White Sauce', '35', 'https://www.pepperdelight.com/wp-content/uploads/2016/01/ChickenPasta1.jpg'),
+(35, 'BBQ', '45', 'https://feastio.com/wp-content/uploads/2023/01/Goldees-BBQ-tray.jpg');
 
 -- --------------------------------------------------------
 
@@ -181,8 +177,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`userID`, `username`, `password`, `address`, `email`, `orders`, `user_type`, `image`) VALUES
-(16, 'jayani', '202cb962ac59075b964b07152d234b70', '385-A/4, new road', 'jayaniadikari19@gmail.com', '', 'user', 'girl-7630191_640.jpg'),
-(17, 'adikari', '202cb962ac59075b964b07152d234b70', '385-A/4, new road', 'adikari@gmail.com', '', 'admin', 'girl-7630191_640.jpg');
+(20, 'adikari', '202cb962ac59075b964b07152d234b70', '385-A/4, new road', 'adikari@gmail.com', '', 'admin', 'girl-7630191_640.jpg'),
+(22, 'jk', '202cb962ac59075b964b07152d234b70', '385-A/4, new road', 'jk@gmail.com', '', 'user', 'girl-7630191_640.jpg'),
+(23, 'user', '202cb962ac59075b964b07152d234b70', '385-A/4, new road', 'user@gmail.com', '', 'user', 'girl-7630191_640.jpg');
 
 --
 -- Indexes for dumped tables
@@ -238,25 +235,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=157;
 
 --
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `userID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
